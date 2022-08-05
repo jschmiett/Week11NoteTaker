8e10813339873fc6dda835861bf6e607fa8751b8
Jennifer Schmiett <bailliejm@gmail.com>
2022-08-04 21:06:40 -0500

    Updated server with html routes and packages

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
-
+const path = require('path');
+const fs = require('fs');
 const db = require('./db/db.json');
-
+const util = require('util');
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
 const app = express();
 
 const PORT = 3001;
@@ -16,31 +19,29 @@ app.use(express.static('public'));
 
 //API Route GET /api/notes to read the db.json file and return all saved notes as JSON
 app.get('./api/notes', (request, response) => {
-    response.json{ notes }
+    // response.json(notes)
 });
 
 //API Route POST /api/notes to receive a new note to save on the request body, and then return the new note to the client.
 app.post('./api/notes', (request, response) => {
     //append new note to the request body
-    newNote = body.append(notes)
-    res.send($newNote)
+    //     newNote = body.append(notes)
+    // res.send($newNote)
 });
 
 //HTML Route GET /notes to return notes.html
-app.get('./public/notes.html', (request, response) => {
-    response.notes.html
+app.get('/notes', (request, response) => {
+    response.sendFile(path.join(__dirname, '/public/notes.html'));
 
 });
 
 //HTML Route GET * to return index.html
-app.get('./public/index.html', (request, response) => {
-    response.notes.html
+app.get('/', (request, response) => {
+    response.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 
 //Each note must have an unique id when saved (npm package)
-
-
 
 
 app.listen(PORT, () =>
